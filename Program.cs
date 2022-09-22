52a4d026a6ab6257975dc769eb0b32781ab563b8
Nikolay <lean01992@gmail.com>
2022-09-22 13:12:05 +0500

    Добавлена основная процедура

--- Program.cs
+++ Program.cs
@@ -6,8 +6,33 @@ namespace FinalHomework
     {
         static void Main(string[] args)
         {
+            static void Changestr(string[] strings)
+        {
             
-            
+            int count = 0;
+            foreach(var item in strings)
+            {
+                if (item.Length <= 3)
+                {
+                   
+                    count++;
+                }
+            }
+            string[] NewStrings = new string[count];
+            count = 0;
+            foreach (var item in strings)
+            {
+                if (item.Length <=3)
+                {
+                    NewStrings[count] = item;
+                    Console.WriteLine(NewStrings[count]);
+                    count++;
+                }
+            }
+           
+         
+        }    
+           
         }
     }
 }
