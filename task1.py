907ca8b7463637d3ea8eec5a787f502da47e1dd1
Nickolay <114084794+FedNick99@users.noreply.github.com>
2022-10-12 11:03:33 +0500

    Delete task1.py

--- task1.py
+++
@@ -1,9 +0,0 @@
-workday = [1,2,3,4,5]
-weekend = [6,7]
-day = int(input("enter a day of week "))
-if day in workday:
-    print("workday")
-elif day in weekend:
-    print("weekend")
-else:
-    print("It's not a day of week")
