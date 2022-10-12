ee111a2dc7dd49531edcdbbc167bf087de97acc1
Nickolay <114084794+FedNick99@users.noreply.github.com>
2022-10-12 11:03:47 +0500

    Delete task2.py

--- task2.py
+++
@@ -1,7 +0,0 @@
-print("x y z")
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            if not (x or y or z) == (not x) and (not y) and (not z):
-                print(x,y,z)
-
